7910088bd493bda7399f1c3694ffabf20f86c55b
mackee <macopy123@gmail.com>
2022-05-18 19:29:23 +0900

    実装を予定しているAPI一覧を定義した

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -43,6 +43,26 @@ func main() {
 	e.Use(middleware.Recover())
 
 	dummy := func(c echo.Context) error { return nil }
+	// for admin endpoint
+	e.POST("/api/tenants/add", dummy)
+	e.GET("/api/tenants/billing", dummy)
+
+	// for tenant endpoint
+	// 参加者操作
+	e.POST("/api/competitors/add", dummy)
+	e.POST("/api/competitor/:competitior_id/disqualified", dummy)
+	// 大会操作
+	e.POST("/api/competitions/add", dummy)
+	e.POST("/api/competition/:competition_id/finish", dummy)
+	e.GET("/api/competition/:competition_id/result", dummy)
+	// テナント操作
+	e.GET("/api/tenant/billing", dummy)
+	// 参加者からの閲覧
+	e.GET("/api/competitor/:competitor_id", dummy)
+	e.GET("/api/competition/:competition_id/ranking", dummy)
+	e.GET("/api/competitions", dummy)
+
+	// for benchmarker
 	e.POST("/initialize", dummy)
 
 	var err error
