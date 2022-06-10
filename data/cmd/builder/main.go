52671f1441302cdb34a877bcc84aba1d7da46dba
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-06-10 22:50:30 +0900

    addわすれ

---
+++ data/cmd/builder/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/isucon/isucon12-qualify/data"
+)
+
+func main() {
+	os.Exit(run())
+}
+
+func run() int {
+	flag.Parse()
+	if len(flag.Args()) != 1 {
+		log.Println("Usage: builder <tenants_num>")
+		return 1
+	}
+	tenantsNum, err := strconv.Atoi(flag.Args()[0])
+	if err != nil {
+		log.Println("Invalid tenants_num:", err)
+		return 1
+	}
+	if err := data.Run(tenantsNum); err != nil {
+		log.Println("ERROR:", err)
+		return 1
+	}
+	return 0
+}
