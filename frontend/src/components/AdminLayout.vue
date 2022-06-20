6301672b43f72901454048bc047069e9437e0d87
Masatoshi Kawazoe (acidlemon) <acidlemon@beatsync.net>
2022-06-20 15:04:21 +0900

    adminにログインできるよ

---
+++ frontend/src/components/AdminLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="main">
+    <div class="header">
+      <HeaderBar />
+    </div>
+    <div class="body">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HeaderBar from '@/components/admin/HeaderBar.vue'
+
+export default defineComponent({
+  name: 'AdminLayout',
+  components: {
+    HeaderBar,
+  },
+})
+</script>
+
+
+<style scoped>
+.main {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 40px 1fr;
+}
+
+.header {
+}
+
+.body {
+}
+
+</style>
---
+++ frontend/src/components/admin/HeaderBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header-bar">
+    <h1 class="brand">ISUports Admin Panel</h1>
+  </div>
+</template>
+
+<style scoped>
+.header-bar {
+  background: orange;
+  color: white;
+  text-align: left;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 4px 12px 0;
+}
+
+.brand {
+  display: inline-block;
+  font-size: 18px;
+  line-height: 32px;
+  margin: 0;
+  padding: 0;
+}
+
+</style>
