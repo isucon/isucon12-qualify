d4364a9c93bd193e3efd60b6909bb71b5e8d67a8
ToshihitoKon <shiratori991@gmail.com>
2022-06-29 15:13:20 +0900

    playerはとってきたrankingからplayerにアクセスするようにした

--- bench/scenario_player.go
+++ bench/scenario_player.go
@@ -53,24 +53,64 @@ func (sc *Scenario) PlayerScenario(ctx context.Context, step *isucandar.Benchmar
 	}
 
 	var competitions []isuports.CompetitionDetail
-	{
-		res, err := GetPlayerCompetitionsAction(ctx, playerAg)
-		v := ValidateResponse("テナント内の大会情報取得", step, res, err, WithStatusCode(200),
-			WithSuccessResponse(func(r ResponseAPICompetitions) error {
-				competitions = r.Data.Competitions
-				return nil
-			}),
-		)
-		if v.IsEmpty() {
-			sc.AddScoreByScenario(step, ScoreGETPlayerCompetitions, scTag)
-		} else {
-			return v
+	for {
+		{
+			res, err := GetPlayerCompetitionsAction(ctx, playerAg)
+			v := ValidateResponse("テナント内の大会情報取得", step, res, err, WithStatusCode(200),
+				WithSuccessResponse(func(r ResponseAPICompetitions) error {
+					competitions = r.Data.Competitions
+					return nil
+				}),
+			)
+			if v.IsEmpty() {
+				sc.AddScoreByScenario(step, ScoreGETPlayerCompetitions, scTag)
+			} else {
+				return v
+			}
 		}
+
+		// NOTE: worker発火直後はcompetitionsが無いので登録されるまで待つ
+		if len(competitions) != 0 {
+			break
+		}
+		sleepms := rand.Intn(500)
+		time.Sleep(time.Millisecond * time.Duration(sleepms))
 	}
 
-	for i := 0; i < 100; i++ {
-		if rand.Intn(100) < 30 {
-			res, err := GetPlayerAction(ctx, playerID, playerAg)
+	// 大会を一つ選ぶ
+	loopCount := 10
+	for i := 0; i < loopCount; i++ {
+		compIndex := rand.Intn(len(competitions))
+		comp := competitions[compIndex]
+		playerIDs := []string{}
+
+		{
+			res, err := GetPlayerCompetitionRankingAction(ctx, comp.ID, "", playerAg)
+			v := ValidateResponse("大会内のランキング取得", step, res, err, WithStatusCode(200),
+				WithSuccessResponse(func(r ResponseAPICompetitionRanking) error {
+					for _, rank := range r.Data.Ranks {
+						playerIDs = append(playerIDs, rank.PlayerID)
+					}
+					return nil
+				}),
+			)
+			if v.IsEmpty() {
+				sc.AddScoreByScenario(step, ScoreGETPlayerRanking, scTag)
+			} else {
+				return v
+			}
+		}
+
+		if len(playerIDs) == 0 {
+			continue
+		}
+
+		// 大会参加者をn人くらい見る
+		maxPlayerCount := 10
+		playerCount := rand.Intn(maxPlayerCount)
+		for j := 0; j < playerCount; j++ {
+			playerIndex := rand.Intn(len(playerIDs))
+			res, err := GetPlayerAction(ctx, playerIDs[playerIndex], playerAg)
 			v := ValidateResponse("参加者と戦績情報取得", step, res, err, WithStatusCode(200),
 				WithSuccessResponse(func(r ResponseAPIPlayer) error {
 					_ = r
@@ -82,26 +122,11 @@ func (sc *Scenario) PlayerScenario(ctx context.Context, step *isucandar.Benchmar
 			} else {
 				return v
 			}
-		} else {
-			for _, comp := range competitions {
-				{
-					res, err := GetPlayerCompetitionRankingAction(ctx, comp.ID, "", playerAg)
-					v := ValidateResponse("大会内のランキング取得", step, res, err, WithStatusCode(200),
-						WithSuccessResponse(func(r ResponseAPICompetitionRanking) error {
-							_ = r
-							return nil
-						}),
-					)
-					if v.IsEmpty() {
-						sc.AddScoreByScenario(step, ScoreGETPlayerRanking, scTag)
-					} else {
-						return v
-					}
-				}
-			}
 		}
-		time.Sleep(time.Millisecond * time.Duration(rand.Intn(1000)))
-
 	}
+
+	sleepms := rand.Intn(1000)
+	time.Sleep(time.Millisecond * time.Duration(sleepms))
+
 	return nil
 }
