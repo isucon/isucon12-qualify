0841a285f5b4c62f6fa09e9a72a62d8279a57c5b
ToshihitoKon <shiratori991@gmail.com>
2022-07-14 15:26:05 +0900

    step-summaryを諦める

--- development/step-summary.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -u
-
-PR_NUMBER=$(echo ${GITHUB_REF_NAME} | sed -e 's/\/merge//')
-if [ "$(echo $PR_NUMBER | sed -e 's/[0-9]//g')" -ne "" ]; then
-    exit 0
-fi
-PR_LINK=${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/pull/${PR_NUMBER}
-echo '### [PR \#${PR_NUMBER}](${PR_LINK})' >> $GITHUB_STEP_SUMMARY
