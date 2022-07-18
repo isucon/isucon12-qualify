42ced7986928714bd8aa946e87fc5253a4a572bc
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-07-18 18:35:18 +0900

    nginx default serverを作っておく
    bot対策

--- provisioning/mitamae/cookbooks/nginx/default.rb
+++ provisioning/mitamae/cookbooks/nginx/default.rb
@@ -14,6 +14,13 @@ remote_file "/etc/nginx/conf.d/isuports.conf" do
   source "isuports.conf"
 end
 
+remote_file "/etc/nginx/conf.d/default.conf" do
+  owner "root"
+  group "root"
+  mode "0644"
+  source "default.conf"
+end
+
 remote_directory "/etc/nginx/tls" do
   owner "root"
   group "root"
