43469cb848647ce3e4139f19477418bf70c26c70
mackee <macopy123@gmail.com>
2022-07-19 10:16:15 +0900

    てにをはが間違ってた

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -644,8 +644,8 @@ func tenantsBillingHandler(c echo.Context) error {
 	}
 	// テナントごとに
 	//   大会ごとに
-	//     scoreに登録されているplayer * 100
-	//     scoreに登録されていないplayerでアクセスした人 * 10
+	//     scoreが登録されているplayer * 100
+	//     scoreが登録されていないplayerでアクセスした人 * 10
 	//   を合計したものを
 	// テナントの課金とする
 	ts := []TenantRow{}
