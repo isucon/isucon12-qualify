1ff9bafa91712669dfdee5713eff9be891a76fa8
Kohei Suzuki <eagletmt@gmail.com>
2022-07-19 21:59:09 +0900

    fix(ruby): Follow #186
    Co-authored-by: mackee <macopy123@gmail.com>

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -412,9 +412,8 @@ module Isuports
 
       # テナントごとに
       #   大会ごとに
-      #     scoreに登録されているplayerでアクセスした人 * 100
-      #     scoreに登録されているplayerでアクセスしていない人 * 50
-      #     scoreに登録されていないplayerでアクセスした人 * 10
+      #     scoreが登録されているplayer * 100
+      #     scoreが登録されていないplayerでアクセスした人 * 10
       #   を合計したものを
       # テナントの課金とする
       tenant_billings = []
