3e0cfc5c85cd5de09238832182179dd259330c92
Masatoshi Kawazoe (acidlemon) <acidlemon@beatsync.net>
2022-07-19 10:47:43 +0900

    feat(node) Cache-Control: private

--- webapp/node/src/main.ts
+++ webapp/node/src/main.ts
@@ -258,6 +258,10 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use((_req: Request, res: Response, next: NextFunction) => {
+  res.set('Cache-Control', 'private')
+  next()
+})
 app.set('etag', false)
 
 const upload = multer()
