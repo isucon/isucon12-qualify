a8a53b3033873359cac6464cf5c550a18e82b68f
Okada Shohei <okashoicircus0@gmail.com>
2022-07-21 07:01:46 +0900

    fix(php): リハサール環境で整合性チェックに失敗するので SQLite3 のコネクション作成時の ATTR_PERSISTENT オプションを指定しないように修正
    また、他のオプションもデフォルト値なので、オプションを明示的に渡さないようにした
    
    see: https://github.com/isucon/isucon12-qualify/pull/153#issuecomment-1190030206

--- webapp/php/src/Isuports/Handlers.php
+++ webapp/php/src/Isuports/Handlers.php
@@ -62,11 +62,6 @@ class Handlers
             params: [
                 'path' => $this->tenantDBPath($id),
                 'driver' => 'pdo_sqlite',
-                'driverOptions' => [
-                    PDO::ATTR_PERSISTENT => true,
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                ],
             ],
             config: $this->sqliteConfiguration,
         );
