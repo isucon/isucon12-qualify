f54fd104064b72d9b270d7a2aeaec1bec37297ae
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-07-21 14:57:24 +0900

    add rust Dockerfile

--- provisioning/mitamae/cookbooks/webapp/build.rb
+++ provisioning/mitamae/cookbooks/webapp/build.rb
@@ -1,8 +1,14 @@
-%w[go python ruby php perl node].each do |lang|
+%w[go python ruby php perl node java rust].each do |lang|
   execute "build webapp #{lang}" do
     command "docker compose -f docker-compose-#{lang}.yml build"
     user 'isucon'
     cwd '/home/isucon/webapp'
     not_if { File.exist?("/.dockerenv") }
+  end
+
+  directory "/home/isucon/tmp/#{lang}" do
+    owner 'isucon'
+    group 'isucon'
+    mode '1777'
   end
 end
