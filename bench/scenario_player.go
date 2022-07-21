e7338660bc7fa03b16f60ffb095ada2156e4ff01
ToshihitoKon <shiratori991@gmail.com>
2022-07-21 17:32:01 +0900

    好きなタイミングでworkerを落とせる

--- bench/scenario_player.go
+++ bench/scenario_player.go
@@ -29,14 +29,15 @@ func (sc *Scenario) PlayerScenarioWorker(step *isucandar.BenchmarkStep, p int32,
 		scTag += "HeavyTenant"
 	}
 
-	w, err := worker.NewWorker(func(ctx context.Context, _ int) {
-		if err := sc.PlayerScenario(ctx, step, scTag, tenantName, playerID); err != nil {
-			sc.ScenarioError(scTag, err)
-			SleepWithCtx(ctx, SleepOnError)
-		}
-	},
-		// 無限回繰り返す
-		worker.WithInfinityLoop(),
+	w, err := worker.NewWorker(
+		func(ctx context.Context, _ int) {
+			if err := sc.PlayerScenario(ctx, step, scTag, tenantName, playerID); err != nil {
+				sc.ScenarioError(scTag, err)
+				SleepWithCtx(ctx, SleepOnError)
+				AdminLogger.Println("PlayerAPIのエラーにより参加者が減りました")
+			}
+		},
+		worker.WithLoopCount(1),
 		worker.WithMaxParallelism(1),
 	)
 	if err != nil {
@@ -58,105 +59,115 @@ func (sc *Scenario) PlayerScenario(ctx context.Context, step *isucandar.Benchmar
 		return err
 	}
 
-	var competitions []isuports.CompetitionDetail
 	for {
-		res, err, txt := GetPlayerCompetitionsAction(ctx, playerAg)
-		msg := fmt.Sprintf("%s %s", playerAc, txt)
-		v := ValidateResponseWithMsg("テナント内の大会情報取得", step, res, err, msg, WithStatusCode(200),
-			WithSuccessResponse(func(r ResponseAPICompetitions) error {
-				competitions = r.Data.Competitions
-				return nil
-			}),
-		)
-		if v.IsEmpty() {
-			sc.AddScoreByScenario(step, ScoreGETPlayerCompetitions, scTag)
-		} else {
-			sc.AddErrorCount()
-			return v
-		}
-
-		if len(competitions) != 0 {
-			break
-		}
-
-		// NOTE: worker発火直後はcompetitionsが無いので登録されるまで待つ
-		sleepms := 500 + rand.Intn(500)
-		SleepWithCtx(ctx, time.Millisecond*time.Duration(sleepms))
-	}
-
-	for i := 0; i < ConstPlayerScenarioCompetitionLoopCount; i++ {
-		// 大会を一つ選ぶ
-		// 開催中の大会があれば90%でそれを選ぶ
-		compIndex := -1
-		if rand.Intn(100) < 90 {
-			for i, comp := range competitions {
-				if !comp.IsFinished {
-					compIndex = i
-					break
-				}
-			}
-		}
-
-		// なければなんでも良いので一つ選ぶ
-		if compIndex < 0 {
-			compIndex = rand.Intn(len(competitions))
-		}
-		comp := competitions[compIndex]
-
-		playerIDs := []string{}
-		rankAfterCheck := rand.Intn(100) < 20
-		rankAfter := ""
+		var competitions []isuports.CompetitionDetail
+		// 大会を取得する
 		for {
-			res, err, txt := GetPlayerCompetitionRankingAction(ctx, comp.ID, rankAfter, playerAg)
+			res, err, txt := GetPlayerCompetitionsAction(ctx, playerAg)
 			msg := fmt.Sprintf("%s %s", playerAc, txt)
-			v := ValidateResponseWithMsg("大会内のランキング取得", step, res, err, msg, WithStatusCode(200),
-				WithSuccessResponse(func(r ResponseAPICompetitionRanking) error {
-					for _, rank := range r.Data.Ranks {
-						playerIDs = append(playerIDs, rank.PlayerID)
-					}
+			v := ValidateResponseWithMsg("テナント内の大会情報取得", step, res, err, msg, WithStatusCode(200),
+				WithSuccessResponse(func(r ResponseAPICompetitions) error {
+					competitions = r.Data.Competitions
 					return nil
 				}),
 			)
 			if v.IsEmpty() {
-				sc.AddScoreByScenario(step, ScoreGETPlayerRanking, scTag)
+				sc.AddScoreByScenario(step, ScoreGETPlayerCompetitions, scTag)
+			} else if v.Canceled {
+				return nil
+			} else if v.Canceled {
+				// contextの打ち切りでloopを抜ける
+				return nil
 			} else {
 				sc.AddErrorCount()
 				return v
 			}
-			if !rankAfterCheck || len(playerIDs) == 0 {
+
+			if len(competitions) != 0 {
 				break
 			}
-			rankAfterCheck = false // rankAfterCheckは一回だけで良い
-			rankAfter = strconv.Itoa(rand.Intn(len(playerIDs) - 1))
-			AdminLogger.Println("DEBUG: ranking after check", rankAfter)
-		}
 
-		if len(playerIDs) == 0 {
-			sleepms := randomRange([]int{500, 1000})
+			// NOTE: worker発火直後はcompetitionsが無いので登録されるまで待つ
+			sleepms := 500 + rand.Intn(500)
 			SleepWithCtx(ctx, time.Millisecond*time.Duration(sleepms))
-			continue
 		}
 
-		// 大会参加者を何人か見る
-		playerCount := rand.Intn(ConstPlayerScenarioMaxPlayerCount)
-		for j := 0; j < playerCount; j++ {
-			playerIndex := rand.Intn(len(playerIDs))
-			res, err, txt := GetPlayerAction(ctx, playerIDs[playerIndex], playerAg)
-			msg := fmt.Sprintf("%s %s", playerAc, txt)
-			v := ValidateResponseWithMsg("参加者と戦績情報取得", step, res, err, msg, WithStatusCode(200),
-				WithSuccessResponse(func(r ResponseAPIPlayer) error {
-					_ = r
-					return nil
-				}),
-			)
-			if v.IsEmpty() {
-				sc.AddScoreByScenario(step, ScoreGETPlayerDetails, scTag)
-			} else {
-				sc.AddErrorCount()
-				return v
+		for i := 0; i < ConstPlayerScenarioCompetitionLoopCount; i++ {
+			// 大会を一つ選ぶ
+			// 開催中の大会があれば90%でそれを選ぶ
+			compIndex := -1
+			if rand.Intn(100) < 90 {
+				for i, comp := range competitions {
+					if !comp.IsFinished {
+						compIndex = i
+						break
+					}
+				}
 			}
-			sleepms := randomRange([]int{1000, 2000})
-			SleepWithCtx(ctx, time.Millisecond*time.Duration(sleepms))
+
+			// なければなんでも良いので一つ選ぶ
+			if compIndex < 0 {
+				compIndex = rand.Intn(len(competitions))
+			}
+			comp := competitions[compIndex]
+
+			playerIDs := []string{}
+			rankAfterCheck := rand.Intn(100) < 20
+			rankAfter := ""
+			for {
+				res, err, txt := GetPlayerCompetitionRankingAction(ctx, comp.ID, rankAfter, playerAg)
+				msg := fmt.Sprintf("%s %s", playerAc, txt)
+				v := ValidateResponseWithMsg("大会内のランキング取得", step, res, err, msg, WithStatusCode(200),
+					WithSuccessResponse(func(r ResponseAPICompetitionRanking) error {
+						for _, rank := range r.Data.Ranks {
+							playerIDs = append(playerIDs, rank.PlayerID)
+						}
+						return nil
+					}),
+				)
+				if v.IsEmpty() {
+					sc.AddScoreByScenario(step, ScoreGETPlayerRanking, scTag)
+				} else if v.Canceled {
+					return nil
+				} else {
+					sc.AddErrorCount()
+					return v
+				}
+				if !rankAfterCheck || len(playerIDs) == 0 {
+					break
+				}
+				rankAfterCheck = false // rankAfterCheckは一回だけで良い
+				rankAfter = strconv.Itoa(rand.Intn(len(playerIDs)))
+			}
+
+			// 大会参加者を何人か見る
+			// もし参加者がいなければ大会を選び直す
+			if len(playerIDs) == 0 {
+				continue
+			}
+
+			playerCount := randomRange([]int{5, 10})
+			for j := 0; j < playerCount; j++ {
+				playerIndex := rand.Intn(len(playerIDs))
+				res, err, txt := GetPlayerAction(ctx, playerIDs[playerIndex], playerAg)
+				msg := fmt.Sprintf("%s %s", playerAc, txt)
+				v := ValidateResponseWithMsg("参加者と戦績情報取得", step, res, err, msg, WithStatusCode(200),
+					WithSuccessResponse(func(r ResponseAPIPlayer) error {
+						_ = r
+						return nil
+					}),
+				)
+				if v.IsEmpty() {
+					sc.AddScoreByScenario(step, ScoreGETPlayerDetails, scTag)
+				} else if v.Canceled {
+					return nil
+				} else {
+					sc.AddErrorCount()
+					return v
+				}
+				sleepms := randomRange([]int{1000, 2000})
+				SleepWithCtx(ctx, time.Millisecond*time.Duration(sleepms))
+			}
 		}
 	}
 
