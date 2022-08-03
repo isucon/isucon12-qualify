b27b57617041efac03bc69dad2b650aec29b7223
ToshihitoKon <shiratori991@gmail.com>
2022-08-03 17:35:09 +0900

    sleepを追加

--- bench/scenario_player.go
+++ bench/scenario_player.go
@@ -55,7 +55,9 @@ func (sc *Scenario) PlayerScenarioWorker(step *isucandar.BenchmarkStep, p int32,
 	}, nil
 }
 
+// 本来意図していた挙動版
 func (sc *Scenario) PlayerScenario(ctx context.Context, step *isucandar.BenchmarkStep, scTag ScenarioTag, tenantName, playerID string) error {
+	AdminLogger.Println("new")
 	report := timeReporter(string(scTag))
 	defer report()
 	sc.ScenarioStart(scTag)
@@ -68,36 +70,34 @@ func (sc *Scenario) PlayerScenario(ctx context.Context, step *isucandar.Benchmar
 	SlowResponseCount := 0
 
 	for {
+	RETRY_CHOOSE_COMP:
 		var competitions []isuports.CompetitionDetail
 		// 大会を取得する
-		for {
-			res, err, txt := GetPlayerCompetitionsAction(ctx, playerAg)
-			msg := fmt.Sprintf("%s %s", playerAc, txt)
-			v := ValidateResponseWithMsg("テナント内の大会情報取得", step, res, err, msg, WithStatusCode(200),
-				WithSuccessResponse(func(r ResponseAPICompetitions) error {
-					competitions = r.Data.Competitions
-					return nil
-				}),
-			)
-			if v.IsEmpty() {
-				sc.AddScoreByScenario(step, ScoreGETPlayerCompetitions, scTag)
-			} else if v.Canceled {
+		res, err, txt := GetPlayerCompetitionsAction(ctx, playerAg)
+		msg := fmt.Sprintf("%s %s", playerAc, txt)
+		v := ValidateResponseWithMsg("テナント内の大会情報取得", step, res, err, msg, WithStatusCode(200),
+			WithSuccessResponse(func(r ResponseAPICompetitions) error {
+				competitions = r.Data.Competitions
 				return nil
-			} else if v.Canceled {
-				// contextの打ち切りでloopを抜ける
-				return nil
-			} else {
-				sc.AddErrorCount()
-				return v
-			}
+			}),
+		)
+		if v.IsEmpty() {
+			sc.AddScoreByScenario(step, ScoreGETPlayerCompetitions, scTag)
+		} else if v.Canceled {
+			return nil
+		} else if v.Canceled {
+			// contextの打ち切りでloopを抜ける
+			return nil
+		} else {
+			sc.AddErrorCount()
+			return v
+		}
 
-			if len(competitions) != 0 {
-				break
-			}
-
-			// NOTE: worker発火直後はcompetitionsが無いので登録されるまで待つ
+		// 大会がなければsleepして選び直す
+		if len(competitions) == 0 {
 			sleepms := 500 + rand.Intn(500)
 			SleepWithCtx(ctx, time.Millisecond*time.Duration(sleepms))
+			goto RETRY_CHOOSE_COMP
 		}
 
 		for i := 0; i < ConstPlayerScenarioCompetitionLoopCount; i++ {
@@ -119,6 +119,8 @@ func (sc *Scenario) PlayerScenario(ctx context.Context, step *isucandar.Benchmar
 			}
 			comp := competitions[compIndex]
 
+			// 大会ランキングを取得する
+			// 20%の確率でrankAfterをつけてもう一度リクエストする
 			playerIDs := []string{}
 			rankAfterCheck := rand.Intn(100) < 20
 			rankAfter := ""
@@ -157,16 +159,16 @@ func (sc *Scenario) PlayerScenario(ctx context.Context, step *isucandar.Benchmar
 				}
 				rankAfterCheck = false // rankAfterCheckは一回だけで良い
 				rankAfter = strconv.Itoa(rand.Intn(len(playerIDs)))
+				sleepms := rand.Intn(500)
+				SleepWithCtx(ctx, time.Millisecond*time.Duration(sleepms))
 			}
 
 			// 大会参加者を何人か見る
-			// もし参加者がいなければ大会を選び直す
-			// 	NOTE: 予選後のメモ
-			// 		予選開催時、考慮漏れのため「空のランキングが返ってきた場合（スコア入稿が完了していない場合）はsleep無しでranking取得を繰り返す」という挙動になっていた
-			// 		そのため想定から外れ、PlayerAPIをほぼ叩かずrankingを叩いてスコアを稼ぐという解法があった。
-			// 		ただし、スコア入稿の速度によってrankingを叩く回数が大きくブレるため、ベンチマークスコアのブレも大きくなる傾向にあった。
+			// 参加者がいなければ大会一覧の取得からやり直す
 			if len(playerIDs) == 0 {
-				continue
+				sleepms := 500 + rand.Intn(500)
+				SleepWithCtx(ctx, time.Millisecond*time.Duration(sleepms))
+				goto RETRY_CHOOSE_COMP
 			}
 
 			playerCount := randomRange([]int{3, 5})
@@ -200,6 +202,7 @@ func (sc *Scenario) PlayerScenario(ctx context.Context, step *isucandar.Benchmar
 
 // 以下は予選開催時の状態
 func (sc *Scenario) PlayerScenarioReproduce(ctx context.Context, step *isucandar.BenchmarkStep, scTag ScenarioTag, tenantName, playerID string) error {
+	AdminLogger.Println("old")
 	report := timeReporter(string(scTag))
 	defer report()
 	sc.ScenarioStart(scTag)
